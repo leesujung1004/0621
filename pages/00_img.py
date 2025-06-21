91c51bb5805b2e7d35d44f0105a0660d39c6299a
leesujung1004 <shop55149221@gmail.com>
2025-06-21 13:19:47 +0900

    Create 00_img.py

---
+++ pages/00_img.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+# 이미지 출력 (GIF 포함)
+st.image("https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExc21hZX1dmVyazJ2dm1jMnBndGh1NTRjMGcweG1jdXgzOGhraG05MSZ1cD12MV9pbnR1cm5hbF9naWZyYn1faw@my3Q9zw/qG2aJyWDOLWtJ2r9cJ/giphy.gif")
